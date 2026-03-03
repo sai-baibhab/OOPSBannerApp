75894908db46e06e6c56b3946c1324a4578902ca
saibaibhab <cm7777@srmist.edu.in>
2026-03-03 08:42:38 +0530

    Added UC7 using Map

--- UC7PrintBanner.java
+++ UC7PrintBanner.java
@@ -71,7 +71,7 @@ public class BannerApp {
                 " ***** "
         };
 
-        // Create objects
+       
         CharacterPatternMap oPattern =
                 new CharacterPatternMap('O', O);
 
